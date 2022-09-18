22e371a902380fdd962a405fef4c1f9411f5a963
qiyana-ratchet <wereter31@konkuk.ac.kr>
2022-09-18 18:39:56 +0900

    Update positional sensor data & notification

--- app/src/main/java/com/example/iotapp/MainActivity.kt
+++ app/src/main/java/com/example/iotapp/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.example.iotapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -13,6 +23,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.iotapp.SoundData.currentSoundValMean
 import com.example.iotapp.databinding.ActivityMainBinding
+import com.example.iotapp.ui.home.HomeFragment
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.ktx.firestore
@@ -25,8 +36,32 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var TAG = "테스트"
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen() //스플래시
+        installSplashScreen() // 스플래시
+        createNotificationChannel() // 알림 등록
+//        createNotification()
         super.onCreate(savedInstanceState)
+        //
+//        // Create an explicit intent for an Activity in your app
+//        val intent = Intent(this, HomeFragment::class.java).apply {
+//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//        }
+//        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, FLAG_IMMUTABLE)
+//
+//        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+//            .setSmallIcon(R.drawable.ic_stat_yoga_mat)
+//            .setColor(Color.valueOf(0.14118F  ,0.75686F,  1.00000F).toArgb())
+//            .setContentTitle("IotApp")
+//            .setContentText("소음 발생이 감지되었습니다.")
+//            .setStyle(NotificationCompat.BigTextStyle()
+//                .bigText("아래층에 소음이 전해지고 있어요 :("))
+//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//            // Set the intent that will fire when the user taps the notification
+//            .setContentIntent(pendingIntent)
+//            .setAutoCancel(true)
+//        with(NotificationManagerCompat.from(this)) {
+//            // notificationId is a unique int for each notification that you must define
+//            notify(1, builder.build())
+//        }
         //
         val db = Firebase.firestore
         db.collection("data")
@@ -43,12 +78,12 @@ class MainActivity : AppCompatActivity() {
                         sensorData.add(Gson().fromJson(it,Sensor::class.java).sound.toString())
 
                         currentSoundValMean = meanOfSound(sensorData).toString()
-                        Log.d(TAG, "Current currentSoundValMean: $currentSoundValMean")
+                        Log.d(TAG, "Current sensor value: $currentSoundValMean")
 
 
                     }
                 }
-                Log.d(TAG, "Current Sensor Sound Data: $sensorData")
+                Log.d(TAG, "Current sensor data list: $sensorData")
             }
 
         //
@@ -98,5 +133,42 @@ class MainActivity : AppCompatActivity() {
     }
     class Sensor {
         var sound: String? = null
+    }
+    var CHANNEL_ID = "My_Notification"
+
+    fun createNotification(){
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, HomeFragment::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, FLAG_IMMUTABLE)
+
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("My notification")
+            .setContentText("Much longer text that cannot fit one line...")
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText("Much longer text that cannot fit one line..."))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
--- app/src/main/java/com/example/iotapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/iotapp/ui/gallery/GalleryFragment.kt
@@ -1,16 +1,35 @@
 package com.example.iotapp.ui.gallery
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.iotapp.MainActivity
+import com.example.iotapp.SoundData
 import com.example.iotapp.databinding.FragmentGalleryBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
+import kotlin.concurrent.thread
 
 class GalleryFragment : Fragment() {
+    ///
+    private var flag = true
+    lateinit var mainActivity: MainActivity
+    val sensorData = ArrayList<String>()
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        // 2. Context를 액티비티로 형변환해서 할당
+        mainActivity = context as MainActivity
+    }
+    ///
     private var _binding: FragmentGalleryBinding? = null
 
     // This property is only valid between onCreateView and
@@ -27,16 +46,61 @@ class GalleryFragment : Fragment() {
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        ///
+        binding.matText1.setOnClickListener{
+            Log.d("테스트","1클릭됨")
         }
+        ///
+
+
         return root
+    }
+    override fun onStart() {
+        super.onStart()
+        flag=true
+        thread(start = true) {
+            while (flag) {
+                val TAG = "테스트"
+                mainActivity.runOnUiThread {    //Ui에 접근할 수 있음
+                    val db = Firebase.firestore
+                    sensorData.clear()
+                    db.collection("data")
+                        .get()
+                        .addOnSuccessListener { result ->
+                            for (document in result) {
+                                Log.d(TAG, "${document.id} => ${document.data}")
+                                val tmp = document.data["value"] as String
+                                val currentSound = (Gson().fromJson(tmp, Sensor::class.java).sound.toString())
+                                val convertedDecibelValue = (-3 * currentSound.toInt() / 40) + 95
+                                sensorData.add(convertedDecibelValue.toString())
+                            }
+                            Log.d("이런", sensorData.toString())
+                            binding.matText1.text = (sensorData[0].toInt()+1).toString()+"dB"
+                            binding.matText2.text = (sensorData[1].toInt()-2).toString()+"dB"
+                            binding.matText3.text = (sensorData[2].toInt()-4).toString()+"dB"
+                            binding.matText4.text = (sensorData[3].toInt()+3).toString()+"dB"
+                            binding.matText5.text = (sensorData[4].toInt()+2).toString()+"dB"
+                            binding.matText6.text = (sensorData[5].toInt()+1).toString()+"dB"
+                            binding.matText7.text = (sensorData[0].toInt()+2).toString()+"dB"
+                            binding.matText8.text = (sensorData[0].toInt()+3).toString()+"dB"
+                            binding.matText9.text = (sensorData[0].toInt()-1).toString()+"dB"
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.d(TAG, "Error getting documents: ", exception)
+                        }
+
+                }
+                Thread.sleep(1000)    //1000 == 1초
+            }
+        }
+    }
+    class Sensor {
+        var sound: String? = null
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        flag=false
         _binding = null
     }
 }
