19267e3b606d210961ae38fe4262ffc5b7d7522a
qiyana-ratchet <wereter31@konkuk.ac.kr>
2022-09-21 01:10:30 +0900

    Add bar chart

--- app/src/main/java/com/example/iotapp/MainActivity.kt
+++ app/src/main/java/com/example/iotapp/MainActivity.kt
@@ -124,6 +124,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
     fun meanOfSound(list: ArrayList<String>): Int {
         var sum = 0
         for (num in list) {
--- app/src/main/java/com/example/iotapp/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/iotapp/ui/slideshow/SlideshowFragment.kt
@@ -1,27 +1,27 @@
 package com.example.iotapp.ui.slideshow
 
-import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.iotapp.MainActivity
 import com.example.iotapp.R
-import com.example.iotapp.SoundData
 import com.example.iotapp.databinding.FragmentSlideshowBinding
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.google.gson.Gson
-import java.text.SimpleDateFormat
-import kotlin.concurrent.thread
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+
 
 class SlideshowFragment : Fragment() {
 
-
+    var barChart: BarChart? = null
     private var _binding: FragmentSlideshowBinding? = null
 
     // This property is only valid between onCreateView and
@@ -39,11 +39,118 @@ class SlideshowFragment : Fragment() {
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setChartView(binding.root)
+
 
         return root
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    private fun setChartView(view: View) {
+        var chartWeek = view.findViewById<BarChart>(R.id.chart_week)
+        setWeek(chartWeek)
+    }
+
+    private fun initBarDataSet(barDataSet: BarDataSet) {
+        //Changing the color of the bar
+        barDataSet.color = Color.parseColor("#304567")
+        //Setting the size of the form in the legend
+        barDataSet.formSize = 15f
+        //showing the value of the bar, default true if not set
+        barDataSet.setDrawValues(true)
+        //setting the text size of the value of the bar
+        barDataSet.valueTextSize = 15f
+    }
+
+    private fun setWeek(barChart: BarChart) {
+        initBarChart(barChart)
+
+        barChart.setScaleEnabled(false) //Zoom In/Out
+
+        val valueList = ArrayList<Double>()
+        val entries: ArrayList<BarEntry> = ArrayList()
+        val title = "걸음 수"
+
+        //input data
+        for (i in 0..5) {
+            valueList.add(i * 100.1)
+        }
+
+        //fit the data into a bar
+        for (i in 0 until valueList.size) {
+            val barEntry = BarEntry(i.toFloat(), valueList[i].toFloat())
+            entries.add(barEntry)
+        }
+        val barDataSet = BarDataSet(entries, title)
+        val data = BarData(barDataSet)
+        ///
+        initBarDataSet(barDataSet)
+        ///
+        barChart.data = data
+        barChart.invalidate()
+    }
+
+    private fun initBarChart(barChart: BarChart) {
+
+        //hiding the grey background of the chart, default false if not set
+        barChart.setDrawGridBackground(false)
+        //remove the bar shadow, default false if not set
+        barChart.setDrawBarShadow(false)
+        //remove border of the chart, default false if not set
+        barChart.setDrawBorders(false)
+
+        //remove the description label text located at the lower right corner
+        val description = Description()
+        description.setEnabled(false)
+        barChart.setDescription(description)
+
+        //X, Y 바의 애니메이션 효과
+        barChart.animateY(1000)
+        barChart.animateX(1000)
+
+
+        //바텀 좌표 값
+        val xAxis: XAxis = barChart.getXAxis()
+        //change the position of x-axis to the bottom
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        //set the horizontal distance of the grid line
+        xAxis.granularity = 1f
+        xAxis.textColor = Color.RED
+        //hiding the x-axis line, default true if not set
+        xAxis.setDrawAxisLine(false)
+        //hiding the vertical grid lines, default true if not set
+        xAxis.setDrawGridLines(false)
+
+
+        //좌측 값 hiding the left y-axis line, default true if not set
+        val leftAxis: YAxis = barChart.getAxisLeft()
+        leftAxis.setDrawAxisLine(false)
+        leftAxis.textColor = Color.RED
+
+
+        //우측 값 hiding the right y-axis line, default true if not set
+        val rightAxis: YAxis = barChart.getAxisRight()
+        rightAxis.setDrawAxisLine(false)
+        rightAxis.textColor = Color.RED
+
+
+        //바차트의 타이틀
+        val legend: Legend = barChart.getLegend()
+        //setting the shape of the legend form to line, default square shape
+        legend.form = Legend.LegendForm.LINE
+        //setting the text size of the legend
+        legend.textSize = 11f
+        legend.textColor = Color.YELLOW
+        //setting the alignment of legend toward the chart
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        //setting the stacking direction of legend
+        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        //setting the location of legend outside the chart, default false if not set
+        legend.setDrawInside(false)
     }
 }
