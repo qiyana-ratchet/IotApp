189a2d325f20c2d49520467fa28ec0ee1ed66c0d
qiyana-ratchet <wereter31@konkuk.ac.kr>
2022-09-26 11:42:37 +0900

    Add vod menu

--- app/src/main/java/com/example/iotapp/MainActivity.kt
+++ app/src/main/java/com/example/iotapp/MainActivity.kt
@@ -108,7 +108,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_vod
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
